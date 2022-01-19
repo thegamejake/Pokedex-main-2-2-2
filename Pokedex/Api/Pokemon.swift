c9294198e8e016ea72c02dfe20df3e3e92901093
tony <usengjake@gmail.com>
2022-01-19 14:31:57 +0800

    Initial Commit

---
+++ Pokedex/Api/Pokemon.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+
+struct Pokemon : Codable{
+    var results: [PokemonEntry]
+}
+
+struct PokemonEntry : Codable, Identifiable  {
+    var id = UUID()
+    var name: String
+    var url: String
+}
+
+class PokeApi  {
+    func getData(completion:@escaping ([PokemonEntry]) -> ()) {
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=151") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            
+            let pokemonList = try! JSONDecoder().decode(Pokemon.self, from: data)
+            
+            DispatchQueue.main.async {
+                completion(pokemonList.results)
+            }
+        }.resume()
+    }
+}
---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
